e264a0d2fcecbb4818c8a7090cbd1790eec64bf1
konschier <maykonschier@gmail.com>
2021-03-18 23:14:47 -0300

    useReducer ensinamentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Reducer, useReducer, useRef } from 'react';
+
+enum ActionTypes {
+  add = 'add/todo',
+  remove = 'remove/todo',
+  reset = 'reset/todo'
+}
+
+interface TodoProps {
+  id: number;
+  value: string;
+}
+interface StateProps {
+  todo: Array<TodoProps>;
+}
+
+interface ActionProps {
+  type: ActionTypes;
+  value?: any;
+}
+
+const initialState: StateProps = { todo: [] } as StateProps;
+
+const reducer: Reducer<StateProps, ActionProps> = (state, action) => {
+  switch(action.type) {
+    case ActionTypes.add:
+      return { ...state, todo: [...state.todo, action.value] };
+    case ActionTypes.remove:
+      return { ...state, todo: state.todo.filter(({ id }) => id !== action.value) };
+    case ActionTypes.reset:
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [{ todo }, dispatch] = useReducer(reducer, initialState);
+
+  function addTodo() {
+    dispatch({ 
+      type: ActionTypes.add,
+      value: {
+        id: new Date().getTime(),
+        value: inputRef.current?.value
+      },
+    })
+  }
+
+  function removeItem(id: number) {
+    dispatch({ type: ActionTypes.remove, value: id })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={inputRef} type="text" />
+      <button onClick={addTodo}>Salvar</button>
+      <br />
+      <ul>
+        {todo.map(({ id, value }) => <li onClick={() => removeItem(id)} key={id}>{value}</li>)}
+      </ul>
     </div>
   );
 }
